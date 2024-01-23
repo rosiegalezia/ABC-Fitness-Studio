2439b16c45aa35ab4d13476535b62a7a9a6f2a9d
rosiegalezia <rosegalezia@gmail.com>
2024-01-23 17:30:12 +0000

    updated logo and added styling and responsiveness to cards

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+// Date in footer
+
+const year = document.querySelector('#current-year')
+
+year.textContent = new Date().getFullYear()
--- README.md
+++ README.md
@@ -14,6 +14,7 @@ The full brief can be viewed [here](./Brief.md).
 - [License](#license)
 
 ## Installation
+N/A
 
 ## Usage
 
