cefef3cc8ee3094400fbd80c4ff4e2f5f7e11e3e
rosiegalezia <rosegalezia@gmail.com>
2024-01-29 18:40:41 +0000

    built out the class and browse classes functions

--- react/src/Pages/BrowseClasses.jsx
+++ react/src/Pages/BrowseClasses.jsx
@@ -1,5 +1,17 @@
-function BrowseClasses(){
+import Class from "../Components/Class"
+import fitnessClasses from "../assets/Class Data/classData.json"
 
+import '../assets/styles.css'
+
+function BrowseClasses() {
+    return <div>
+        <h2 className="p-5 pb-3">Browse our classes</h2>
+        <div className="flex-wrap d-flex m-4 p-4">
+            {fitnessClasses.map((fitnessClass) => {
+                return <fitnessClass key={fitnessClass.id} name={fitnessClass.name} image={fitnessClass.image} description={fitnessClass.description} instructor={fitnessClass.instructor} duration={fitnessClass.duration} />;
+            })}
+        </div>
+    </div>
 }
 
 export default BrowseClasses
